4e120a10d1eecffe18388e5a742dd1d295018708
Saravanakumar <saravanakumar323py@gmail.com>
2019-04-24 22:45:48 +0530

    refs #master Acceptance test commit

--- WebApp-Continuous-Delivery/TodoApp/src/TodoApp/settings/base.py
+++ WebApp-Continuous-Delivery/TodoApp/src/TodoApp/settings/base.py
@@ -78,21 +78,10 @@ WSGI_APPLICATION = 'TodoApp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-#DATABASES = {
- #   'default': {
-  #      'ENGINE': 'django.db.backends.sqlite3',
-   #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    #}
-#}
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql', 
-        'NAME': 'todo',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
--- WebApp-Continuous-Delivery/TodoApp/src/TodoApp/settings/test.py
+++ WebApp-Continuous-Delivery/TodoApp/src/TodoApp/settings/test.py
@@ -1,8 +1,23 @@
+from .base import *
 import os
-from base import *
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+INSTALLED_APPS += ('django_nose',)
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+TEST_OUTPUT_DIR = os.environ.get('TEST_OUTPUT_DIR','.')
+NOSE_ARGS = [
+'--verbosity=2',
+'--nologcapture',
+'--with-coverage',
+'--cover-package=todo',
+'--with-spec',
+'--spec-color',
+'--with-xunit',
+'--xunit-file=%s/unittests.xml'% TEST_OUTPUT_DIR,
+'--cover-xml',
+'--cover-xml-file=%s/coverage.xml'% TEST_OUTPUT_DIR
+]
 
 DATABASES = {
     'default': {
